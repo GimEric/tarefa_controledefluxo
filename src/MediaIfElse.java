278f401f590cc9e987bcdc16e4568edb1c70154f
eric gimenes <gimenes.eric@live.com>
2024-01-03 13:59:30 -0300

    first commit

--- src/MediaIfElse.java
+++ src/MediaIfElse.java
@@ -1,5 +1,4 @@
 public class MediaIfElse {
-
     public static void main(String[] args) {
 
         double num1 = 10;
@@ -7,16 +6,15 @@ public class MediaIfElse {
         double num3 = 8;
         double num4 = 4;
         double  media = (num1 + num2 + num3 + num4) /4 ;
-
         System.out.println(media);
 
         if (media >=7) {
             System.out.println("Está Aprvado");
         }
-        else if (media <7 && media>5) {
+        else if (media <7 && media>=5) {
             System.out.println("Refazer teste");
         }
-        else if (media<+4) {
+        else if (media<=4) {
             System.out.println("Reprovado");
         }
     }
